fc77dcb1d223db59f13ce317d6f96724955d2c83
N33to <pedro.arruda@outlook.com.br>
2023-12-04 20:06:41 -0300

    Adding windows installation script

---
+++ windows/installation.ps1
@@ -0,0 +1,22 @@
+# Execute as admin
+# Execute this commando to give permission to run scripts
+# Set-ExecutionPolicy Unrestricted
+
+winget install Adobe.Acrobat.Reader.64-bit --silent;
+winget install Discord.Discord --silent; 
+winget install ElectronicArts.EADesktop --silent;
+winget install Git.Git --silent;
+winget install Google.Chrome --silent;
+winget install Microsoft.VisualStudioCode --silent;
+winget install Mozilla.Firefox --silent;
+winget install Guru3D.Afterburner --silent;
+winget install Nvidia.GeForceExperience --silent;
+winget install OBSProject.OBSStudio --silent;
+winget install Obsidian.Obsidian --silent;
+winget install Microsoft.PowerToys --silent;
+winget install qBittorrent.qBittorrent --silent;
+winget install Spotify.Spotify --silent;
+winget install Valve.Steam --silent;
+winget install VideoLAN.VLC --silent;
+winget install VMware.WorkstationPlayer --silent;
+winget install RARLab.WinRAR --silent;
